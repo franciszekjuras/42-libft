4a6e21b8818647d4bbfb8ce84c7bde655291f3e1
fjuras <fjuras@student.42wolfsburg.de>
2022-04-27 14:52:58 +0200

    Fix division by zero and header for linux

--- ft_alloc.c
+++ ft_alloc.c
@@ -6,19 +6,19 @@
 /*   By: fjuras <fjuras@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 17:38:55 by fjuras            #+#    #+#             */
-/*   Updated: 2022/04/25 20:04:34 by fjuras           ###   ########.fr       */
+/*   Updated: 2022/04/27 14:47:41 by fjuras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <limits.h>
+#include <stdint.h>
 #include "libft.h"
 
 void	*ft_calloc(size_t number, size_t size)
 {
 	void	*mem;
 
-	if (SIZE_MAX / size < number)
+	if (size != 0 && SIZE_MAX / size < number)
 		return (0);
 	mem = malloc(number * size);
 	if (mem == 0)
